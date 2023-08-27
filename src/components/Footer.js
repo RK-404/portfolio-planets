358e7e94d63e731337e48e9cac7516b796a18d5d
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-08-27 12:26:53 -0400

    Added my Social Links to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,10 @@ export default function Footer() {
   return (
     <footer>
       <p>*Data collected from <a href="https://solarsystem.nasa.gov/planets/" target="_blank" rel="noreferrer">NASA</a></p>
+      <p>Developed by RIDWAN 👉
+        <a href="https://github.com/RK-404/" target="_blank" rel="noreferrer"> GitHub </a> |
+        <a href="https://www.linkedin.com/in/mdridwanrobin/" target="_blank" rel="noreferrer"> LinkedId</a>
+      </p>
     </footer>
   );
 }
